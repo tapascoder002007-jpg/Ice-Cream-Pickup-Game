36dfc43ce5b59cc5a5f19d5401fad512ff25c672
Tapas Dev Yadav <tapascoder002007@gmail.com>
2026-05-31 12:44:12 +0530

    The projecto

---
+++ Assets/Script/Driver.cs
@@ -0,0 +1,63 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float currentSpeed = 5f;
+    [SerializeField] float steerSpeed = 220f;
+    [SerializeField] float boostSpeed = 10f;
+    [SerializeField] float regularSpeed = 5f;
+    [SerializeField] TMP_Text boostText;
+    void Start()
+    {
+        boostText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steer = 0f;
+        float move = 0f;
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        float moveAmount= move*currentSpeed*Time.deltaTime;
+        float steerAmount = steer*steerSpeed*Time.deltaTime;
+
+
+        transform.Translate(0,moveAmount,0);
+        transform.Rotate(0,0,steerAmount);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Boost"))
+        {
+            currentSpeed = boostSpeed;
+            boostText.gameObject.SetActive(true);
+            Destroy(collision.gameObject);
+        }
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentSpeed = regularSpeed;
+        boostText.gameObject.SetActive(false);
+    }
+}
